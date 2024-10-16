fefc5d08d695fd52a487c8e2323dd1c06ea5e372
dinozin <dinozindev@gmail.com>
2024-10-16 15:53:47 -0300

    flask 06

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -11,6 +11,21 @@ jogo2 = Jogo('Skyrim', 'Aventura', 'PC')
 jogo3 = Jogo('God Of War', 'Rack n Slash', 'PS2')
 lista = [jogo1, jogo2, jogo3]
 
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+
+usuario1 = Usuario("Bruno Divino", "BD", "alohomora")
+usuario2 = Usuario("Camila Ferreira", "Mila", "paozinho")
+usuario3 = Usuario("Guilherme Louro", "Cake", "python_eh_vida")
+usuarios = {
+    usuario1.nickname : usuario1,
+    usuario2.nickname : usuario2,
+    usuario3.nickname : usuario3
+}
+
 app = Flask(__name__)
 app.secret_key = 'alura'
 
@@ -18,9 +33,13 @@ app.secret_key = 'alura'
 def index():
     return render_template('lista.html', titulo="Jogos", jogos=lista)
 
+# se não estiver logado, redireciona para login, passando a varivavel 'proxima' como novo (Quando o login terminar queremos retornar a pagina novo)
 @app.route('/novo')
 def novo():
-    return render_template('novo.html', titulo='Novo Jogo')
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    else:
+        return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
 def criar():
@@ -29,26 +48,32 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
+# pega os args da requisição /login e armazena na variável proxima, que será armazenada em value do input da página login.html
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
+# recebe o valor de proxima do input de login.html e faz o redirect concatenando, que nesse caso o resultado será "/novo"
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'alohomora' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(session['usuario_logado'] + ' logado com sucesso!')
-        return redirect('/')
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
         flash('Usuário não logado.')
-        return redirect('/login')  
+        return redirect(url_for('login'))  
 
 @app.route('/logout') 
 def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
-    return redirect('/')    
+    return redirect(url_for('index'))    
 
 app.run(debug=True)
